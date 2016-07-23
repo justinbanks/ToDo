b942fe8b8ce0b61206b808d57f252cfb081d09aa
Jarrett <jokerknight670@gmail.com>
2016-07-23 17:46:41 -0500

    added search and sort buttons

--- app/src/main/java/com/todo/group1/todo/MainActivity.java
+++ app/src/main/java/com/todo/group1/todo/MainActivity.java
@@ -9,6 +9,8 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -113,5 +115,13 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.extra_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
